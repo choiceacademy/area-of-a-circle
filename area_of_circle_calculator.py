c637b8c89c453111d9f2c80f8e482d1081bb9713
Choice Academy <58896504+choiceacademy@users.noreply.github.com>
2019-12-23 13:05:40 -0500

    Add files via upload

---
+++ area_of_circle_calculator.py
@@ -0,0 +1,7 @@
+import math
+print("Welcome to the area of a circle calculator! Imput any whole, positive number radius and I will calculate its area!")
+i = int(input ("Please enter a radius: "))
+k= 3.14159216*i*i
+print ("The area of your circle is: " ,k, "\t")
+z = round(k,0)
+print ("Rounded, that's: ", z)
